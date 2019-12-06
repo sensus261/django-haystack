a02be8efbcb030416491bbcbc76bdb7e2c99b70f
sensus261 <44163811+sensus261@users.noreply.github.com>
2019-12-06 14:21:45 +0200

    Update __init__.py
    Remove django.six, that has been deprecated with Django2

--- haystack/utils/__init__.py
+++ haystack/utils/__init__.py
@@ -6,7 +6,6 @@ import importlib
 import re
 
 from django.conf import settings
-from django.utils import six
 
 from haystack.constants import ID, DJANGO_CT, DJANGO_ID
 from haystack.utils.highlighting import Highlighter
