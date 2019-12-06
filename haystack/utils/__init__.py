1ff53e1ef4260313ff425ae3c2e7ba3b762d8dfd
sensus261 <44163811+sensus261@users.noreply.github.com>
2019-12-06 14:24:03 +0200

    Update __init__.py
    imported pyhton's six dependecy

--- haystack/utils/__init__.py
+++ haystack/utils/__init__.py
@@ -6,6 +6,7 @@ import importlib
 import re
 
 from django.conf import settings
+import six
 
 from haystack.constants import ID, DJANGO_CT, DJANGO_ID
 from haystack.utils.highlighting import Highlighter
